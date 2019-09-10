b6033158fad4994371c36942c327ca39c9eaa8e9
nkgrnkgr <beefupme@gmail.com>
2019-09-10 21:42:07 +0900

    update mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import React, { useState } from 'react';
 import { Route, withRouter } from 'react-router-dom';
 import { RouteComponentProps } from 'react-router';
+import { Toolbar } from '@material-ui/core';
 import { PortFolioLayout } from './components/PortfolioLayout';
 import { HomeLayout } from './components/HomeLayout';
 import './App.css';
 import { PageHeader } from './components/PageHeader';
+import { ScrollTop } from './components/ScrollTop';
 
 type AppProps = RouteComponentProps;
 
-const App: React.FC<AppProps> = ({ history }) => {
+const App: React.FC<AppProps> = props => {
   const [activeMenuIndex, setActiveMenuIndex] = useState(0);
   const menuItems = ['home', 'portfolio', 'slide', 'links'];
 
   const handleOnClickMenu = (menuIndex: number) => {
     setActiveMenuIndex(menuIndex);
-    history.push(menuItems[menuIndex]);
+    props.history.push(menuItems[menuIndex]);
   };
 
   return (
@@ -24,9 +26,11 @@ const App: React.FC<AppProps> = ({ history }) => {
         activeMenuIndex={activeMenuIndex}
         handleOnClickMenu={handleOnClickMenu}
       />
+      <Toolbar id="back-to-top-anchor" />
       <Route path="/" exact component={HomeLayout} />
       <Route path="/home" exact component={HomeLayout} />
       <Route path="/portfolio" component={PortFolioLayout} />
+      <ScrollTop {...props} />
     </div>
   );
 };
