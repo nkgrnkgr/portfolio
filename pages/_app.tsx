1d1b9095f4ffcc354edb6c07d55ee4b4fde33e10
taiki-nishi <taiki-nishi@cybozu.co.jp>
2021-09-22 13:29:28 +0900

    material-ui のHEADを追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import React from 'react'
 
 const App: React.VFC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <>
+    <Head>
+      <title>Nokogiri</title>
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
+    </Head>
+    <Component {...pageProps} />
+  </>
 )
 
 export default App
